9668e3628322e70f5c5e243128510e8a6adb71a6
suharaaa <suha.lyoko@hotmail.com>
2020-03-31 03:20:43 +0530

    student controller

--- controllers/students.controllers.js
+++ controllers/students.controllers.js
@@ -1,2 +1,23 @@
 const student = require('../models/student.model')
 
+const enrollStudent = (req, res) => {
+    //create student
+    const student = new Student(req.body);
+
+    //save to db
+    student.save().then(result => {
+        res.status(200).json({
+            success: true,
+            data: result
+        });
+    }).catch(err => {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    });
+}
+
+module.exports = {
+    enrollStudent
+};
