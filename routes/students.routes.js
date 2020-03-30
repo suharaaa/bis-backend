70a6e3a6779fd8ce82320894bccf0edf4b8a6077
suharaaa <suha.lyoko@hotmail.com>
2020-03-31 00:24:36 +0530

    routes student

--- routes/students.routes.js
+++ routes/students.routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
 // define routes here
-app.post('/student/add', taskController.addStudent);
+// app.post('/student/add', taskController.addStudent);
 
 module.exports = router;
