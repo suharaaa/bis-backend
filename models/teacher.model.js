4d16f3e199f41f21a0285110cea9cf89e80b161f
Ruvindi Guanthilake <ruvigunathilake@gmail.com>
2020-04-05 03:24:50 +0530

    Nothing

--- models/teacher.model.js
+++ models/teacher.model.js
@@ -20,4 +20,4 @@ const teacherSchema = new mongoose.Schema({
 
 },  { timestamps: true });
 
-module.exports = mongoose.model('Teacher', teacherSchema);
+module.exports = mongoose.model('teachers', teacherSchema);
