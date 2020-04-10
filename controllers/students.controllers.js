110a81408771b685d59f028e289a020062084952
suharaaa <suha.lyoko@hotmail.com>
2020-04-10 16:43:27 +0530

    remove a student

--- controllers/students.controllers.js
+++ controllers/students.controllers.js
@@ -181,11 +181,23 @@ const updateStudent = (req, res) => {
 const deleteStudentById = (req, res) => {
 
     Student.findByIdAndDelete(req.params.id)
-    .populate('class')
-    .then(result => {
-        res.status(200).json({
-            success: true,
-            data: result
+    .then(s => {
+        Classes.findOneAndDelete({
+            students: mongoose.Types.ObjectId(req.params.id)
+        }, {
+            $pullAll: {
+                students: [mongoose.Types.ObjectId(req.params.id)]
+            }
+        }).then(result => {
+            res.status(200).json({
+                success: true,
+                data: result
+            });
+        }).catch(err => {
+            res.status(500).json({
+                success: false,
+                message: err.message
+            });
         });
     }).catch(err => {
         res.status(500).json({
@@ -193,6 +205,7 @@ const deleteStudentById = (req, res) => {
             message: err.message
         });
     });
+
 };
 
 const getNextAdmissionNumber = (req, res) => {
