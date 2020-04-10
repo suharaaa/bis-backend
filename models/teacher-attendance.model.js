a4bfdc2f2afaa9ceb130391ebc0750b57f733bc0
Chamodi Thennakoon <chamodithennakoon97@gmail.com>
2020-04-10 06:09:10 +0530

    Implemented teacher attendance CRUD

--- models/teacher-attendance.model.js
+++ models/teacher-attendance.model.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const teacherAttendanceSchema = new Schema({
-});
-
+    teacher: { type: Schema.Types.ObjectId, ref: 'teachers' },
+    date: { type: String, required: true },
+    status: { type: String, enum: ['present', 'onleave'], default: 'present' }
+}, { timestamps: true });
 
 module.exports = mongoose.model('TeacherAttendance', teacherAttendanceSchema);
--- routes/attendances.routes.js
+++ routes/attendances.routes.js
@@ -1,12 +1,17 @@
 const router = require('express').Router();
 const attendanceController = require('../controllers/attendances.controllers');
+const teacherAttendanceController = require('../controllers/teacher-attendance.controller');
 
 // define routes here
+router.get('/attendance/teachers', teacherAttendanceController.getTeacherAttendanceByDate);
+router.post('/attendance/teachers/:id', teacherAttendanceController.createNewTeacherAttendance);
+router.put('/attendance/teachers', teacherAttendanceController.updateTeacherAttendance);
+router.delete('/attendance/:id', teacherAttendanceController.deleteTeacherAttendance);
+
 router.post('/attendance', attendanceController.createAttendance);
 router.get('/attendance', attendanceController.viewAttendance);
 router.get('/attendance/:id', attendanceController.viewAttendanceById);
 router.put('/attendance/:id', attendanceController.updateAttendance);
-router.delete('/attendance/:id', attendanceController.deleteAttendance);
-
+// router.delete('/attendance/:id', attendanceController.deleteAttendance);
 
 module.exports = router;
