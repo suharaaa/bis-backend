6445a2524fa8a16f8264ee36d9290d418b394cdd
Tharushika1996 <48749826+Tharushika1996@users.noreply.github.com>
2020-04-11 17:07:52 +0530

    modified results

--- routes/results.routes.js
+++ routes/results.routes.js
@@ -7,7 +7,8 @@ router.post('/results',resultsControllers.createNewResult);
 router.put('/results/:id',resultsControllers.UpdateResults);
 router.delete('/results/:id',resultsControllers.DeleteResults);
 router.get('/results',resultsControllers.viewResults);
-
+router.get('/results/:id',resultsControllers.findResultID);
+ 
 module.exports = router;
 
 
