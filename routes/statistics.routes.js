137bd1a7de2a7decc5aedb169bac105de73365ce
Suhara Vithanage <suha.lyoko@hotmail.com>
2020-04-12 05:19:05 +0530

    Added chart stats

--- routes/statistics.routes.js
+++ routes/statistics.routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const statisticsController = require('../controllers/statistics.controller');
 
 router.get('/statistics', statisticsController.getStatistics);
+router.get('/statistics/enrollments', statisticsController.getStudentEnrollmentCountsByYear);
 
 module.exports = router;
 
