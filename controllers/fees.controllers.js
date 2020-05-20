68ea9fa5f205bd102f11d13c12681ad738d4851b
AzfaMariyam <57381624+AzfaMariyam@users.noreply.github.com>
2020-05-21 01:01:08 +0530

    new changes on fees

--- controllers/fees.controllers.js
+++ controllers/fees.controllers.js
@@ -98,54 +98,6 @@ const findFeeID = (req, res) =>{
 
 
 
-/*
-//update fees
-const UpdateFee = (req, res) => {
-
-    if( !req.body.grade){  //body has the tasks content, if name isnt defined in body, it gives an error
-
-        return res.status(400).json({
-
-                success : false,
-                message : "Grade is undefined"
-
-        });  //this checks client sde errors
-
-    }
-
-
-    Fees.findByIdAndUpdate(req.params.id, {
-
-
-        grade : req.body.grade,
-        termfee : req.body.termfee,
-        facilityfee : req.body.facilityfee,
-        librarycharges : req.body.librarycharges,
-        laboratorycharges : req.body.laboratorycharges,
-        transportationfee : req.body.transportationfee,
-        other : req.body.other,
-        updatedOn : new Date()
-
-
-    },).then(result => 
-        {
-            res.status(200).json({
-
-                success : true,
-                data : result
-
-        });
-    }).catch(err => {
-            
-        res.status(500).json({
-            success : false,
-            message : err.message
-
-        });    
-   
-    });
-};
-*/
 
 
 
