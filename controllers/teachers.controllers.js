36fe38ad9d8835ecf214157b5c9eaf73a3fb0515
Ruvindi Guanthilake <ruvigunathilake@gmail.com>
2020-05-22 00:03:36 +0530

    history

--- controllers/teachers.controllers.js
+++ controllers/teachers.controllers.js
@@ -171,24 +171,7 @@ const getNextTid = (req, res) => {
 };
 
 
-// //finding
 
-// const viewTeacherId = (req, res) => {
-//     Teacher.findById(req.params.id)
-        
-//         .then(result => {
-//             res.status(200).json({
-//                 success: true,
-//                 data: result
-//             });
-
-//         }).catch(err => {
-//             res.status(500).json({
-//                 success: false,
-//                 message: err.message
-//             });
-//         });
-// };
 
 //searching
 
