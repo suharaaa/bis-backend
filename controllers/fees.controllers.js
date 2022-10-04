45dd96be96eb3830f113271a492c5e0ee542cb4a
unknown <chenukasumanasekara@gmail.com>
2022-10-04 21:55:49 +0530

    Update functionality is changed in Fees

--- controllers/fees.controllers.js
+++ controllers/fees.controllers.js
@@ -37,7 +37,57 @@ const createNewFee = (req, res) => {
 
 }
 
+const UpdateFee = (req, res) => {
 
+    if( !req.body.grade){  //body has the tasks content, if grade isnt defined in body, it gives an error
+
+        return res.status(400).json({
+
+                success : false,
+                message : "Grade is undefined"
+
+        });  //this checks client sde errors
+
+    }
+
+    
+
+   
+    Fees.findByIdAndUpdate(req.params.id, {
+
+
+        
+        grade : req.body.grade,
+        termfee : req.body.termfee,
+        facilityfee : req.body.facilityfee,
+        librarycharges : req.body.librarycharges,
+        laboratorycharges : req.body.laboratorycharges,
+        transportationfee : req.body.transportationfee,
+        other : req.body.other,
+        tot : req.body.tot,
+        updatedOn : new Date()
+      
+        
+
+
+    }).then(result => 
+        {
+            res.status(200).json({
+
+                success : true,
+                data : result
+
+        });
+    }).catch(err => {
+            
+        res.status(500).json({
+            success : false,
+            message : err.message
+
+        });    
+   
+    });
+};
 
 
 //to retrieve the fees that are created
@@ -94,71 +144,7 @@ const findFeeID = (req, res) =>{
    
     });
 
-};
-
-
-
-
-
-
-const UpdateFee = (req, res) => {
-
-    if( !req.body.grade){  //body has the tasks content, if grade isnt defined in body, it gives an error
-
-        return res.status(400).json({
-
-                success : false,
-                message : "Grade is undefined"
-
-        });  //this checks client sde errors
-
-    }
-
-    
-
-   
-    Fees.findByIdAndUpdate(req.params.id, {
-
-
-        
-        grade : req.body.grade,
-        termfee : req.body.termfee,
-        facilityfee : req.body.facilityfee,
-        librarycharges : req.body.librarycharges,
-        laboratorycharges : req.body.laboratorycharges,
-        transportationfee : req.body.transportationfee,
-        other : req.body.other,
-        tot : req.body.tot,
-        updatedOn : new Date()
-      
-        
-
-
-    }).then(result => 
-        {
-            res.status(200).json({
-
-                success : true,
-                data : result
-
-        });
-    }).catch(err => {
-            
-        res.status(500).json({
-            success : false,
-            message : err.message
-
-        });    
-   
-    });
-};
-
-
-
-
-
-
-
+}
 
 
 //delete fee
